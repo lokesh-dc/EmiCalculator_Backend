c6101213b5d8aafdc69747ca929f01922de23fac
lokesh-dc <lokesh.cdewanand@gmail.com>
2022-12-13 12:46:22 +0530

    Emi Calculator

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ app.use(express.json());
 
 app.use("/auth", authController)
 
+
+app.get("/calculateemi", async(req,res)=>{
+    
+    const {amount, interest, tenure} = req.body;
+    try{
+        const r = ( interest / 12 ) / 100;
+        let emi = Math.ceil((+amount * +r * Math.pow((1 + +r), +tenure) ) / (Math.pow((1 + +r),tenure) - 1));
+        const totalPayable = emi * tenure;
+        const interestPayable = totalPayable - amount
+        res.status(200).send({"EMI": emi, totalPayable, interestPayable});
+    }catch(e){
+        res.status(500).send(e.message)
+    }   
+})
+
 app.get("/", async (req, res) =>{
     res.send("Working fine");
 })
-
 
 
 app.listen((8080), async ()=>{
